6354f23acf5fcd32d89dac86de056ac8a5703ac6
MJ <mercyorangi2@gmail.com>
2023-11-09 16:50:55 -0500

    add new models

--- models.py
+++ models.py
@@ -12,6 +12,41 @@ app.config.from_object('config') #in models.py = OK
 db = SQLAlchemy(app) #in models.py = OK
 migrate = Migrate(app, db) #in manage.py + manager == OK
 
+class Actor(db.Model):
+   __tablename__ = 'actors'
+   
+   id = db.Column(db.Integer, primary_key=True)
+   name = db.Column(db.String, nullable=False)
+   age = db.Column(db.Integer, nullable=False)
+   gender = db.Column(db.String, nullable=False)
+   show = db.relationship('Showing', backref='actor', lazy=True)
+
+   def __repr__(self):
+      return f'<NEW ACTOR: {self.id} {self.name} {self.age} {self.gender}>'
+   
+class Movie(db.Model):
+   __tablename__ = 'movies'
+
+   id = db.Column(db.Integer, primary_key=True)
+   title = db.Column(db.String, nullable=False)
+   release_date = db.Column(db.String, nullable=False)
+   showing = db.relationship('Showing', backref='movie', lazy=True)
+
+   def __repr__(self):
+      return f'<NEW MOVIE: {self.id} {self.title} {self.release_date}>'
+   
+class Showing(db.Model):
+   __tablename__ = 'showings'
+
+   id = db.Column(db.Integer, primary_key=True)
+   actor_id = db.Column(db.Integer, db.ForeignKey('actors.id'), nullable=False)
+   movie_id = db.Column(db.Integer, db.ForeignKey('movies.id'), nullable=False)
+   start_time = db.Column(db.String, nullable=False)
+
+   def __repr__(self):
+      return f'<NEW SHOWING: {self.id} {self.actor_id} {self.movie_id} {self.start_time}>'
+
+
 class Venue(db.Model):
     __tablename__ = 'venues'
 
